07624c50d1abcecf5da61f66dbe26f9e404fbd06
riteshv2109 <riteshv2109@gmail.com>
2025-03-28 11:12:33 +0000

    escape sequence character,comments

---
+++ escape.py
@@ -0,0 +1,16 @@
+print("hey i am a good boy \n and i love python")
+# what the fuck
+# hey siri! I Love you
+#  \n is used to break lines to new line
+
+'''
+what the fuck
+hey siri! I Love you
+ \n is used to break lines to new line
+
+'''
+
+print("hey i am \"ritesh\" and i love playing \"badminton\"")
+
+print("hey",5,6,7,sep="*")
+# sep=" " is used to separate print strings or characters 
