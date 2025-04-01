15d80d254c0ecf027265e8004db4a6410d92de30
riteshv2109 <riteshv2109@gmail.com>
2025-04-01 14:55:11 +0000

    input.py

---
+++ input.py
@@ -0,0 +1,9 @@
+# a=input("enter your name:")
+
+x=input("enter first number:")
+y=input("enter second number:")
+print(x+y)
+print(int(x)+int(y))
+
+# python takes the input as string use typecasting to convert 
+# into integer to use for addition
