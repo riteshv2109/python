d0415088155e7a69420574ea7274b0ac6ec30c51
riteshv2109 <riteshv2109@gmail.com>
2025-04-01 14:54:38 +0000

    calculator

---
+++ calculator.py
@@ -0,0 +1,10 @@
+a=50
+b=5
+
+print("the value of ",a,"+",b," is:",a+b)
+print("the value of ",a,"-",b," is:",a-b)
+print("the value of ",a,"*",b," is:",a*b)
+print("the value of ",a,"%",b," is:",a%b)
+
+
+# to replicate(copy) same syntax we use alt+shift+downarrow
