ee0630fbbdb7802ca0b9b036d816d2ccbe83dea9
riteshv2109 <riteshv2109@gmail.com>
2025-04-09 13:03:24 +0000

    good morning

---
+++ gudm.py
@@ -0,0 +1,18 @@
+a=float(input("enter the time:"))
+print("your given time  is:",a)
+
+if(a<12):
+    print("good morning sir!")
+    
+elif (12<= a <16):
+   {
+    print("good afternoon sir!") 
+   }
+elif (16<= a <20):
+     {
+        print("good evening sir!")
+     }
+else :
+     {
+        print("good night sir! ")
+     }    
