423cfb0cfa5d00f53445845d5ee5e53c44c6175e
riteshv2109 <riteshv2109@gmail.com>
2025-04-10 12:11:46 +0000

    lists

---
+++ list.py
@@ -0,0 +1,16 @@
+l=[1,2,3,4,5,"ritesh"]
+print(l)
+print(type(l))
+print(l[5])
+print(l[-3])
+# length of string- -ve index
+if 3 in l:
+    print("yes present")
+    
+else:
+    print("not present") 
+
+
+# comprehensive list
+lst=[i for i in range(4)]
+print(lst) 
