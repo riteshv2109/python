236fe2cd63c57aec9cfec2fd0f8206db67f0a936
riteshv2109 <riteshv2109@gmail.com>
2025-04-10 12:12:00 +0000

    list methods

---
+++ listmethods.py
@@ -0,0 +1,12 @@
+list=[1,5,6,3,5,70,12]
+# list.sort()
+# list.reverse()
+# m=list.copy()
+# print(list.index(3))
+# print(list.count(5))
+# print(m[0])
+# list.insert(3,8)
+m=[23,56,99]
+list.extend(m)
+print(list)
+
