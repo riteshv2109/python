cc6ba21bb567559ab883d7cad1a8db5f6645feb8
riteshv2109 <riteshv2109@gmail.com>
2025-04-14 07:23:25 +0000

    fstrings

---
+++ fstrings.py
@@ -0,0 +1,6 @@
+country="india"
+name="ritesh"
+print(f"hey mt name is {name} & i am from {country}")
+# here f is used by computer to understand F strings.
+# if want to print string with curly braces use double time curly braces
+print(f"hey mt name is {{name}}& i am from {{country}}")
