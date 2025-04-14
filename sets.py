952d115c0138a7505a732653a9e0f26c662cb76a
riteshv2109 <riteshv2109@gmail.com>
2025-04-14 08:26:42 +0000

    sets

---
+++ sets.py
@@ -0,0 +1,14 @@
+s={1,2,3,2,4,5,6,4}
+print(type(s))
+t={}
+print(type(t))
+# here clasas of t is dictionary
+# to create an empty set we need to write as [t=set()]
+r=set()
+print(type(r))
+# here type of r is set
+
+for value in s:             
+    print(value)
+    
+# above loop is used to print the set items
