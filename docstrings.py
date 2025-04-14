d50628d35b9ff9446a166479f4e08b971560ba5f
riteshv2109 <riteshv2109@gmail.com>
2025-04-14 07:23:59 +0000

    docstrings

---
+++ docstrings.py
@@ -0,0 +1,7 @@
+def square(n):
+    '''it takes n as input and gives n*n as output'''
+    # this is a docstring and it isn't ignored just like comments
+    print(n*n)
+    
+square(3) 
+print(square.__doc__)     #it is used to print docstring   
