fe16903cad2bd4212edd02657a95bcd76b0ef93c
riteshv2109 <riteshv2109@gmail.com>
2025-05-03 08:02:05 +0000

    operations on sets

---
+++ opsets.py
@@ -0,0 +1,10 @@
+s1={1,2,5,6}
+s2={3,6,7}
+s3={8,9}
+print(s1.union(s2,s3))
+# union function is used to take union os two sets
+# s1.update(s2,s3)
+# update is used to update the values of set
+print(s1)
+s4=s1.intersection(s2)
+print(s4)
