ad67725cefb5cb952ce19ac6d2b7a857bd6a06f8
riteshv2109 <riteshv2109@gmail.com>
2025-05-08 13:12:02 +0000

    import in python

---
+++ import.py
@@ -0,0 +1,15 @@
+# import math
+# a=3.1223
+# b=math.floor(a)
+# print(b)
+from math import sqrt,floor
+a=int(sqrt(144))
+print(a)
+''' if written (from math import *) * dentoes all inbuilt functions will be 
+installed and  are usable '''
+
+import pandas
+a=dir(pandas)
+print(a)
+''' dir() function is used to view all the functions & variables
+defined in a module'''
