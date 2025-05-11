357ff88b27816709f3f27b4fbfb600fc508c5364
riteshv2109 <riteshv2109@gmail.com>
2025-05-11 11:57:42 +0000

    if name main usage

---
+++ ifnamemain.py
@@ -0,0 +1,10 @@
+import ritesh
+
+ritesh.welcome()
+
+'''this will print welcome function twice as ritesh will execute once then 
+ritesh.welcome will also be executed . to avoid this if name main is used'''
+
+'''agar __name__ ki value __main__ hia to isko ritesh se run kiya jaa rha hai
+to welcome ritesh.py wala run hoga
+agar __name__ ki value ifnamemain hai to welcome ifnamemain wala run hoga'''
